f8434bf216eb6306dffc26d9488bfdc81fa8a005
kaouther <kaoutherboutheldja@gmail.com>
2023-11-25 23:03:35 +0100

    add begin file

---
+++ doublyLinkedList/addEnd.c
@@ -0,0 +1,40 @@
+#include "list.h"
+
+/**
+ * addBegin - function that add node in begining
+ * 
+ * @list: doubly linked list pointer
+ * @val: data for the new node
+ * Return: pointer to doubly linked list
+*/
+
+List* ajoutEnQueue(List *list, int val) 
+{
+    if (list == NULL) {
+        printf("Erreur : list not initialize.\n");
+        exit(EXIT_FAILURE);
+    }
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Erreur Memory can't be allocated.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = val;
+    newNode->next = NULL;
+    
+    if (isEmpty(list))
+    {
+        newNode->prev = NULL;
+        list->head = newNode;
+        list->tail = newNode;
+    } 
+    else
+    {
+        list->tail->next = newNode;
+        newNode->prev = list->tail;
+        list->tail = newNode;
+    }
+    return list;
+}
+
